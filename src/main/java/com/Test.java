cae1d559ff4893c0ade72c893862bdce1c75a4ef
dongsongchao <www.1169889740@qq.com>
2021-10-14 10:10:34 +0800

    测试

---
+++ src/main/java/com/Test.java
@@ -0,0 +1,14 @@
+package com;
+
+import java.net.Socket;
+
+/**
+ * @author songchao
+ * @date 2021/10/14 9:14
+ */
+public class Test {
+    public static void main(String[] args) {
+        Object[] a = new Object[]{new Object(),new Object()};
+        System.out.println(a[0].hashCode());
+    }
+}
