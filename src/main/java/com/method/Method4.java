27f11b25880ca03e6779a227b435fa0a24a884e8
dongsongchao <www.1169889740@qq.com>
2021-10-18 15:53:11 +0800

    翻转数字

--- src/main/java/com/method/Method4.java
+++ src/main/java/com/method/Method4.java
@@ -1,7 +1,6 @@
 package com.method;
 
 
-
 /**
  * 翻转数字
  *
@@ -10,40 +9,36 @@ package com.method;
  */
 public class Method4 {
     public static void main(String[] args) {
-        int i = minInteger2(134);
+        int i = minInteger(-1134);
         System.out.println(i);
     }
 
     public static int minInteger(int num) {
+        String s = String.valueOf(num);
+        int flag = 1;
+        if (num < 0) {
+            s = s.substring(1);
+            flag = -1;
+            System.out.println(s);
+        }
         try {
-            if (num > 0) {
-                StringBuffer str = new StringBuffer(String.valueOf(num));
-                String string = str.reverse().toString();
-                return Integer.parseInt(string);
-            }
-            if (num < 0) {
-                String s = String.valueOf(num);
-                StringBuffer str = new StringBuffer(s.substring(1, s.length()));
-                int i = Integer.parseInt(str.reverse().toString());
-                return -i;
-            } else {
-                return 0;
-            }
+            return Integer.parseInt(new StringBuffer(s).reverse().toString()) * flag;
         } catch (Exception e) {
+            e.printStackTrace();
             return 0;
         }
     }
 
     public static int minInteger2(int num) {
         String string = String.valueOf(num);
-        int flag=1;
+        int flag = 1;
         if (num < 0) {
             flag = -1;
             string = string.substring(1);
         }
         try {
-            return   Integer.parseInt((new StringBuffer(string)).reverse().toString())*flag;
-        }catch (Exception e){
+            return Integer.parseInt((new StringBuffer(string)).reverse().toString()) * flag;
+        } catch (Exception e) {
             return 0;
         }
 
