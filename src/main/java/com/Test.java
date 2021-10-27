a993dfa04ddbaba65eaa830c7b793c8d8977a4fa
dongsongchao <www.1169889740@qq.com>
2021-10-27 14:56:12 +0800

    test

--- src/main/java/com/Test.java
+++ src/main/java/com/Test.java
@@ -1,6 +1,15 @@
 package com;
 
+
+
+import org.apache.commons.lang3.StringUtils;
+
 import java.net.Socket;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author songchao
@@ -8,7 +17,21 @@ import java.net.Socket;
  */
 public class Test {
     public static void main(String[] args) {
-        Object[] a = new Object[]{new Object(),new Object()};
-        System.out.println(a[0].hashCode());
+        String s = "测试字符串方法{operId}的方法{operId2}";
+        Map<Object,String> map = new HashMap<>();
+        map.put("operId", "001");
+        map.put("operId2", "002");
+
+        String[] string = StringUtils.substringsBetween(s, "{", "}");
+        for (int i = 0; i < string.length; i++) {
+           s= s.replace("{"+string[i]+"}",map.get(string[i]));
+        }
+        System.out.println(s);
+
+        System.out.println("------------------");
+
+        System.out.println(LocalDate.now());
+        System.out.println(LocalTime.now());
+
     }
 }
